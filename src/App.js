c807dbec3df4bd05d80458bf3dcca85a23607c48
Andrey Ohiienko <andreyohiienko@gmail.com>
2021-03-28 17:27:54 +0300

    Update header title again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Pull Request Feature</header>
+      <header className="App-header">Pull Request</header>
       <p>Some new description</p>
       <p>Additional description </p>
     </div>
