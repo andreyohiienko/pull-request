4a7d46586f75261346835239c2f20086ae9521a9
andreyohiienko <andreyohiienko@gmail.com>
2021-03-28 17:40:16 +0300

    Revert "Remove additional description"

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Multiple pull requests already exist
-      </header>
+      <header className="App-header">Pull Request Feature</header>
       <p>Some new description</p>
+      <p>Additional description </p>
     </div>
   );
 }
