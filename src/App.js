c25c26d87321a59034a92ff62c270f1c07713cdf
Andrey Ohiienko <andreyohiienko@gmail.com>
2021-03-28 16:26:08 +0300

    Add additional description

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">Pull Request</header>
       <p>Some new description</p>
+      <p>Additional description </p>
     </div>
   );
 }
